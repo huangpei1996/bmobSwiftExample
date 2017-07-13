d915c622cd68e6070ab89f7d308377ccaa7c5622
huangpei1996 <1072955124@qq.com>
2017-07-13 16:06:21 +0800

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  bmobSwiftExample
+//
+//  Created by 雅风 on 2017/4/19.
+//  Copyright © 2017年 yafeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        saveObj()
+    }
+    func saveObj(){
+        
+        let gamescore:BmobObject = BmobObject(className: "GameScore")
+        //score为1200
+        gamescore.setObject(222, forKey: "score")
+        //设置playerName为小明
+        gamescore.setObject("小白", forKey: "playerName")
+        //设置cheatMode为false
+        gamescore.setObject(false, forKey: "cheatMode")
+        //设置age为18
+        gamescore.setObject(19, forKey: "age")
+        gamescore.saveInBackground(resultBlock: {isSuccessful,error in
+            print("可以")
+            if error != nil{
+                //发生错误后的动作
+                print("error is \(error?.localizedDescription)")
+            }else{
+                //创建成功后会返回objectId，updatedAt，createdAt等信息
+                //创建对象成功，打印对象值
+                print("succ")
+            }
+        })
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ h.m
@@ -0,0 +1,9 @@
+//
+//  h.m
+//  bmobSwiftExample
+//
+//  Created by 雅风 on 2017/4/19.
+//  Copyright © 2017年 yafeng. All rights reserved.
+//
+
+#import <BmobSDK/Bmob.h>
